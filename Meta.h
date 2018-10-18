b2ed07591abe80962c166f80a0d7769c090141fc
Peihong Ke <PeihongKe@users.noreply.github.com>
2018-10-18 13:27:06 +0100

    resolve meta data taking into account std::vector

--- Meta.h
+++ Meta.h
@@ -22,3 +22,102 @@ class MetaManager
 public:
 	static void resigsterMeta(const Meta)
 };
+
+template<typename T>
+struct GetTypeName1
+{
+	std::string operator() ()
+	{
+		return "struct typename";
+	}
+};
+
+template<>
+struct GetTypeName1<int>
+{
+	std::string operator() ()
+	{
+		return "int";
+	}
+};
+
+
+template<>
+struct GetTypeName1<double>
+{
+	std::string operator() ()
+	{
+		return "double";
+	}
+};
+
+template<typename T>
+struct GetTypeName1<T*>
+{
+	std::string operator() ()
+	{
+		GetTypeName1<T> subType;
+		return "pointer to " + subType();
+	}
+};
+
+template<typename T>
+struct GetTypeName1<std::vector<T>>
+{
+	std::string operator() ()
+	{
+		GetTypeName1<T> subType;
+		return "vector to " + subType();
+	}
+};
+ 
+
+template<typename T>
+struct GetTypeName1<std::set<T>>
+{
+	std::string operator() ()
+	{
+		GetTypeName1<T> subType;
+		return "set to " + subType();
+	}
+};
+
+
+template<typename T1, typename T2>
+struct GetTypeName1<std::map<T1, T2> >
+{
+	std::string operator() ()
+	{
+		GetTypeName1<T1> subType1;
+		GetTypeName1<T2> subType2;
+		return "map from " + subType1() + " to " + subType2();
+	}
+};
+
+template<typename T>
+T* NullCast(void)
+{
+	return reinterpret_cast<T *>(NULL);
+}
+
+int main1()
+{
+	double a = 1.1;
+	int b = 2;
+	std::vector<int> c;
+
+	GetTypeName1<int*> pointerTypePrinter;
+	GetTypeName1<std::vector<int>> vectorTypePrinter;
+	GetTypeName1<std::vector<std::vector<int>>> vectorvectorTypePrinter;
+	GetTypeName1<std::set<std::vector<int>>> setVectorTypePrinter;
+	GetTypeName1<std::vector<std::set<int>>> vectorSetTypePrinter;
+
+	GetTypeName1<std::map<int, int>> mapPrinter;
+	GetTypeName1<std::map<int, std::vector<int>>> mapVectorPrinter;
+	GetTypeName1<std::map<int, int*>> mapPointerPrinter;
+
+	std::map<int, int> aInstance;
+
+	std::cout << GetTypeName1<decltype(aInstance)>()();
+
+}
